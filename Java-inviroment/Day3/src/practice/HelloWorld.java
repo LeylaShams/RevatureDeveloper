f1d122fb609968c5cff43dc29df543691207c355
LeylaShams <leyla.shams@revature.net>
2021-07-14 16:51:05 -0700

    Adding First java file to Revature repo

---
+++ Java-inviroment/Day3/src/practice/HelloWorld.java
@@ -0,0 +1,15 @@
+package practice;
+//Installing new IDE, SpringTool4
+//just practicing
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HelloWold");
+		System.out.println("Added to repo");
+		System.out.println("The first push");
+
+	}
+
+}
